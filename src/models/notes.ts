d38085b2cd689bf6ab935298d896c04816d428fe
Cairon29 <josewjjunco@gmail.com>
2026-04-28 00:51:37 -0500

    feat: modelos de usuario y notas creados

--- src/models/notes.ts
+++ src/models/notes.ts
@@ -1,10 +1,11 @@
 // @ts-ignore
 import oracledb from "oracledb"
 import { dbConfig } from "../db/config.js";
-import type { ModelResponse, Note } from "../types/notes.js"
+import type { NotesModelResponse, Note } from "../types/notes.js"
+import { db_error_handler } from "../utils/utils.js";
 
 export class NotesModel {
-    static getAll = async (): Promise<ModelResponse> => {
+    static getAll = async (): Promise<NotesModelResponse> => {
         let connection;
 
         try {
@@ -22,12 +23,7 @@ export class NotesModel {
                 message: "Notes retrieved"
             };
         } catch (error) {
-            return {
-                data: { note: null },
-                error: "",
-                status: 200,
-                message: "Notes retrieved"
-            }
+            return await db_error_handler(error, "note");
         } finally {
             if (connection) {
                 try {
@@ -38,7 +34,7 @@ export class NotesModel {
             }
         }
     }
-    static getById = async(id: number): Promise<ModelResponse> => {
+    static getById = async(id: number): Promise<NotesModelResponse> => {
         let connection;
         try {
             connection = await oracledb.getConnection(dbConfig)
@@ -64,70 +60,48 @@ export class NotesModel {
                 message: "Note retrieved"
             };
         } catch (error) {
-            return {
-                data: { note: null },
-                error: error instanceof Error ? error.message : "Unknown error",
-                status: 500,
-                message: "Internal server error"
-            }
+            return await db_error_handler(error, "note");
         }
     } 
-    static create = async (note: Note): Promise<ModelResponse> => {    
+    static create = async (note: Note): Promise<NotesModelResponse> => {    
+        // Oracle logic for creating a note
+        let connection;
         try {
-            // Oracle logic for creating a note
-            let connection;
-            try {
-                connection = await oracledb.getConnection(dbConfig)
-                let query = "INSERT INTO notes (id, title, description, document, fk_user_id) VALUES (notes_seq.NEXTVAL, :title, :description, :document, :user_id) RETURNING id INTO :id";
-                const result = await connection.execute(
-                    query,
-                    {
-                        title: note.title,
-                        description: note.description,
-                        document: note.document,
-                        fk_user_id: note.user_id,
-                    }
-                )
-
-                if (result.rowsAffected === 0) {
-                    return {
-                        status: 500,
-                        data: {note: null},
-                        error: "Failed to create note",
-                        message: "Failed to create note"
-                    }
+            connection = await oracledb.getConnection(dbConfig)
+            let query = "INSERT INTO notes (id, title, description, document, fk_user_id) VALUES (notes_seq.NEXTVAL, :title, :description, :document, :user_id) RETURNING id INTO :id";
+            const result = await connection.execute(
+                query,
+                {
+                    title: note.title,
+                    description: note.description,
+                    document: note.document,
+                    fk_user_id: note.user_id,
                 }
+            )
 
-                await connection.commit();
-
-                return {
-                    status: 201,
-                    data: { note: result.outBinds.id[0] },
-                    error: "",
-                    message: "Note created"
-                }
-            
-            } catch (error) {
-                const rollbackConnection = await oracledb.getConnection(dbConfig);
-                await rollbackConnection.rollback();
+            if (result.rowsAffected === 0) {
                 return {
                     status: 500,
-                    data: { note: null },
-                    error: error instanceof Error ? error.message : "Unknown error",
-                    message: "Internal server error"
+                    data: {note: null},
+                    error: "Failed to create note",
+                    message: "Failed to create note"
                 }
             }
-        } catch (error) {
-            // rollback for db pushing error
+
+            await connection.commit();
+
             return {
-                status: 500,
-                data: { note: null },
-                error: error instanceof Error ? error.message : "Unknown error",
-                message: "Internal server error"
+                status: 201,
+                data: { note: result.outBinds.id[0] },
+                error: "",
+                message: "Note created"
             }
+        
+        } catch (error) {
+            return await db_error_handler(error, "note");
         }
     }
-    static delete = async(id: number): Promise<ModelResponse> => {
+    static delete = async(id: number): Promise<NotesModelResponse> => {
         try {
             let connection; 
             connection = await oracledb.getConnection(dbConfig)
@@ -154,29 +128,32 @@ export class NotesModel {
                 message: "Note created"
             }
         } catch (error) {
-            // rollback for db pushing error
-            try {
-                const rollbackConnection = await oracledb.getConnection(dbConfig);
-                await rollbackConnection.rollback();
-            } catch (error) {
-                return {
-                    status: 500,
-                    data: { note: null },
-                    error: error instanceof Error ? error.message : "Unknown error",
-                    message: "Error during rollback"
-                }
-            }
-
-            return {
-                status: 500,
-                data: { note: null },
-                error: error instanceof Error ? error.message : "Unknown error",
-                message: "Internal server error"
-            }
+            return await db_error_handler(error, "note");
         }
     }
-    static update = async(note: Note): Promise<ModelResponse> => {
-
+    static update = async(note: Note): Promise<NotesModelResponse> => {
+        try {
+            let connection; 
+            connection = await oracledb.getConnection(dbConfig)
+            let query = "UPDATE notes SET title = :title, description = :description, document = :document WHERE id = :id";
+            const result = await connection.execute(
+                query,
+                {
+                    title: note.title,
+                    description: note.description,
+                    document: note.document,
+                    id: note.id
+                }
+            )
+            await connection.commit();
+            return {
+                status: 200,
+                data: { note: result.outBinds.id[0] },
+                error: "",
+                message: "Note updated"
+            }
+        } catch (error) {
+            return await db_error_handler(error, "note");
+        }
     }
-
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,204 @@
-class User {
-    static create = () => {
-        return new User();
+// @ts-ignore
+import oracledb from "oracledb"
+import { dbConfig } from "../db/config.js";
+import { db_error_handler, password_encryptor } from "../utils/utils.js";
+
+// @ts-ignore
+import { bcrypt } from "bcrypt";
+import type { UsersModelResponse, User } from "../types/users.js";
+
+export class UserModel {
+    static getAll = async (): Promise<UsersModelResponse> => {
+        let connection;
+
+        try {
+            connection = await oracledb.getConnection(dbConfig)
+            const result = await connection.execute(
+                'SELECT * FROM users',
+                [],
+                { outFormat: oracledb.OUT_FORMAT_OBJECT }
+            );
+            return {
+                
+                data: result.rows,
+                error: "",
+                status: 200,
+                message: "Users retrieved"
+            };
+        } catch (error) {
+            return await db_error_handler(error, "user");
+        } finally {
+            if (connection) {
+                try {
+                    await connection.close();
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
     }
-    static update = () => {
+    static getById = async(id: number): Promise<UsersModelResponse> => {
+        let connection;
+        try {
+            connection = await oracledb.getConnection(dbConfig)
+            const result = await connection.execute(
+                'SELECT * FROM users WHERE id = :id',
+                [id],                
+                { outFormat: oracledb.OUT_FORMAT_OBJECT }
+            );
+
+            if (result.rows.length === 0) {
+                return {
+                    data: { user: null },
+                    error: "",
+                    status: 404,
+                    message: "User not found"
+                };
+            }
+
+            return {
+                data: result.rows,
+                error: "",
+                status: 200,
+                message: "User retrieved"
+            };
+        } catch (error) {
+            return await db_error_handler(error, "user");
+        }
+    } 
+    static create = async (user: User): Promise<UsersModelResponse> => {    
+        // Oracle logic for creating a user
+        let connection;
+        try {
+
+            if (!user.password) {
+                return {
+                    status: 400,
+                    data: { user: null },
+                    error: "Password is required",
+                    message: "Password is required"
+                }
+             }
+
+            const encryptedPassword = await password_encryptor(user.password);
+            
+            connection = await oracledb.getConnection(dbConfig)
+            let query = "INSERT INTO users (id, full_name, password, email) VALUES (users_seq.NEXTVAL, :full_name, :password, :email) RETURNING id INTO :id";
+            const result = await connection.execute(
+                query,
+                {
+                    full_name: user.full_name,
+                    password: encryptedPassword,
+                    email: user.email
+                }
+            )
+
+            if (result.rowsAffected === 0) {
+                return {
+                    status: 500,
+                    data: {user: null},
+                    error: "Failed to create user",
+                    message: "Failed to create user"
+                }
+            }
+
+            if (result.rowsAffected === 0) {
+                return {
+                    status: 500,
+                    data: {user: null},
+                    error: "Failed to create user",
+                    message: "Failed to create user"
+                }
+            }
+
+            await connection.commit();
+
+            return {
+                status: 201,
+                data: { user: result.outBinds.id[0] },
+                error: "",
+                message: "User created"
+            }
         
+        } catch (error) {
+            return await db_error_handler(error, "user");
+        }
     }
-    static delete = () => {
+    static delete = async(id: number): Promise<UsersModelResponse> => {
+        try {
+            let connection; 
+            connection = await oracledb.getConnection(dbConfig)
+            let query = "DELETE FROM users WHERE id = :id";
+            const result = await connection.execute(
+                query,
+                [id]
+            )
+            
+            if (result.rowsAffected === 0) {
+                return {
+                    status: 404,
+                    data: { user: null },
+                    error: "User not found",
+                    message: "User not found"
+                }
+            }
 
-    }    
-    static getById = () => {
-
+            await connection.commit();
+            return {
+                status: 201,
+                data: { user: result.outBinds.id[0] },
+                error: "",
+                message: "User created"
+            }
+        } catch (error) {
+            return await db_error_handler(error, "user");
+        }
     }
-    static getAll = () => {
+    static update = async(user: User): Promise<UsersModelResponse> => {
+        try {
+            if (!user.id) {
+                return {
+                    status: 400,
+                    data: { user: null },
+                    error: "User ID is required",
+                    message: "User ID is required"
+                }
+            }
 
+            let validatedPassword = ""
+            if (user.password) {
+                validatedPassword = await password_encryptor(user.password);
+            }
+            if (!validatedPassword) {
+                return {
+                    status: 400,
+                    data: { user: null },
+                    error: "Password is required",
+                    message: "Password is required"
+                }
+            }
+
+            let connection; 
+            connection = await oracledb.getConnection(dbConfig)
+            let query = "UPDATE users SET full_name = :full_name, password = :password, email = :email WHERE id = :id";
+            const result = await connection.execute(
+                query,
+                {
+                    full_name: user.full_name,
+                    password: validatedPassword,
+                    email: user.email,
+                    id: user.id
+                }
+            )
+            await connection.commit();
+            return {
+                status: 200,
+                data: { user: result.outBinds.id[0] },
+                error: "",
+                message: "User updated"
+            }
+        } catch (error) {
+            return await db_error_handler(error, "user");
+        }
     }
 }
